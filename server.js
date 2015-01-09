1e31911321db11dff0bd7e8aa3f64d3edc84920c
Gyt <igyteng@gmail.com>
2015-01-09 22:44:10 +0800

    第一个可用的版本

--- server.js
+++ server.js
@@ -8,18 +8,33 @@ app.get('/login', function (req, res) {
     console.log('login');
     console.log(req.query);
     // res.redirect('http://192.168.10.1:2060/wifidog/auth?token=1234567890');
-    res.sendfile('./auth.html');
+    if(req.query.pwd) {
+        if(req.query.pwd === 'junjunjun') {
+            res.redirect('http://192.168.11.1:2060/wifidog/auth?token=1234567890');
+        } else {
+            res.sendfile('./authfail.html');
+        }
+    } else {
+        res.sendfile('./auth.html');
+    }
+    
 });
 
-// app.get('/portal', function (req, res) {
-//     console.log('auth');
-//     console.log(req.query);
-//     res.send('Auth: 1');
-// });
+app.get('/auth', function (req, res) {
+    console.log('auth');
+    console.log(req.query);
+    // if()
+    res.send('Auth: 1');
+});
 
 app.get('/ping', function (req, res) {
     console.log(req.url);
     res.send('Pong');
+});
+
+app.get('/portal', function (req, res) {
+    console.log(req.url);
+    res.sendfile('./authsuccess.html');
 });
 
 // app.get('*', function (req, res) {
