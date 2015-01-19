34a9c07c2eb614501e4cda7ef89677b810ab25d8
Gyt <igyteng@gmail.com>
2015-01-19 22:55:06 +0800

    passport

--- manager/app.js
+++ manager/app.js
@@ -1,8 +1,61 @@
-var express    = require('express');
-var router = express.Router();
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('express-flash');
+var passport = require('passport');
 
-exports.router = router.get('/',
+var router = express.Router();
+var LocalStrategy = require('passport-local').Strategy;
+var route      = require('../db/route.js');
+
+var app = express();
+app.use(cookieParser());
+app.use(session({secret: 'gyteng', cookie: { maxAge: 60000 }}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+// app.use(app.router);
+
+exports.router = router;
+
+router.get('/manager',
     function(req, res) {
-        res.send('GG');
+        res.sendFile('./html/login.html', {
+                root: __dirname
+            },
+            function(err) {
+                if (err) {
+                    res.sendStatus(404);
+                }
+            }
+        );
     }
 );
+
+router.post('/manager',
+  passport.authenticate('local', { successRedirect: '/ggg',
+                                   failureRedirect: '/manager'})
+);
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        route.getRoute(username, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect username.'
+                });
+            }
+            var myUser = JSON.parse(JSON.stringify(user));
+            if (myUser.password !== password) {
+                return done(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return done(null, user);
+
+        });
+    }
+));
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var log4js     = require('log4js');
-var express    = require('express');
+
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('express-flash');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var multer     = require('multer');
 var bodyParser = require('body-parser');
 var config     = require('./config.js').conf;
@@ -14,6 +21,12 @@ var app = express();
 app.use(multer());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(session({secret: 'gyteng', cookie: { maxAge: 60000 }}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 // js and css file
 app.use(express.static(__dirname));
@@ -42,7 +55,57 @@ app.get('/gw_message.php', function(req, res) {
 
 app.get('/plugins/:routeId', user.plugins);
 
-app.use('/', manager.router);
+// app.use('/', manager.router);
+
+app.get('/manager',
+    function(req, res) {
+        res.sendFile('./manager/html/login.html', {
+                root: __dirname
+            },
+            function(err) {
+                if (err) {
+                    res.sendStatus(404);
+                }
+            }
+        );
+    }
+);
+
+app.post('/manager',
+  passport.authenticate('local', { successRedirect: '/ggg',
+                                   failureRedirect: '/manager'})
+);
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        route.getRoute(username, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect username.'
+                });
+            }
+            var myUser = JSON.parse(JSON.stringify(user));
+            if (myUser.password !== password) {
+                return done(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return done(null, user);
+
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 
 
